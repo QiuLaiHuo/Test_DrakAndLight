4ef63135707e3761bd4d8891fa3b6fa3a539101c
QiuLaiHuo <yangbin_630@outlook.com>
2023-12-04 00:17:09 +0800

    2

--- Assets/Script/PlayerConrtoll.cs
+++ Assets/Script/PlayerConrtoll.cs
@@ -1,15 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerConrtoll : MonoBehaviour
+public class PlayerConrtoll: MonoBehaviour
 {
-    [Header("必要组件")]
-    public  PlayerInput inputs;
+    [Header ("必要组件")]
+    public PlayerInput inputs;
     public Rigidbody2D rd;
     public SpriteRenderer sprit;
+    public Animator anim;
 
 
     [Header ("行动参数")]
@@ -18,16 +19,20 @@ public class PlayerConrtoll : MonoBehaviour
     public float DoubleJumpForce;
     [SerializeField]
     private Vector2 SpeedValue;
-    private bool DoubleJump = false;
+    private bool IsDoubleJump = false;
+    private bool IsGround=true;
+
+    //private EdgeCollider2D GroundCheck;
 
     private void Awake ()
     {
         inputs = new PlayerInput ();
         rd = GetComponent<Rigidbody2D> ();
         sprit = GetComponent<SpriteRenderer> ();
-
+        anim = GetComponent<Animator> ();
+       // GroundCheck = GetComponent<EdgeCollider2D> ();
     }
-    void Start()
+    void Start ()
     {
         //inputs.GamePlay.Move.started += Move;
         inputs.GamePlay.Attack.started += Attack;
@@ -38,12 +43,16 @@ public class PlayerConrtoll : MonoBehaviour
     #region 帧调用
     void Update ()
     {
-       SpeedValue =  inputs.GamePlay.Move.ReadValue<Vector2> ();
+        SpeedValue = inputs.GamePlay.Move.ReadValue<Vector2> ();
+       
+        anim.SetFloat ("JumpVelocity",rd.velocity.y);
+        
     }
 
     private void FixedUpdate ()
     {
         Move ();
+
     }
     #endregion
 
@@ -51,34 +60,81 @@ public class PlayerConrtoll : MonoBehaviour
     #region 角色操作方法
     private void Defence (InputAction.CallbackContext obj)
     {
-        
+
     }
 
     private void Jump (InputAction.CallbackContext obj)
     {
-        rd.AddForce (Vector2.up*JumpForce,ForceMode2D.Impulse);
+
+        //TOOD：动画设置
+        if (IsGround)
+        {
+            anim.SetTrigger ("Jump");
+            rd.AddForce (Vector2.up * JumpForce,ForceMode2D.Impulse);
+            IsDoubleJump = false;
+        }
+        else if (!IsGround && !IsDoubleJump)
+        {
+            
+            rd.velocity = new Vector2 (rd.velocity.x,0);
+            anim.SetTrigger ("DoubleJump");
+            rd.AddForce (Vector2.up * DoubleJumpForce,ForceMode2D.Impulse);
+            IsDoubleJump = true;
+        }
+        else if (IsDoubleJump)
+            return;
+            
+
     }
 
     private void Attack (InputAction.CallbackContext obj)
     {
-        
+
     }
 
     private void Move ()
     {
+        
         if (SpeedValue.x < -0.1f)
         {
             sprit.flipX = false;
-           
+            
+
         }
-        else if(SpeedValue.x>0.1f){ sprit.flipX = true;  }
+        else if (SpeedValue.x > 0.1f) { sprit.flipX = true;  }
 
         rd.velocity = new Vector2 (SpeedValue.x * MoveSpeed,rd.velocity.y);
-        Debug.Log (SpeedValue.x);
+        anim.SetFloat ("Move",Mathf.Abs (SpeedValue.x));
+
+        if (IsGround && SpeedValue == Vector2.zero)
+            anim.SetTrigger ("Idle");
     }
     #endregion
 
+    
 
+
+    #region 碰撞检测方法
+    private void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.CompareTag("Ground"))
+        {
+            IsGround = true;
+            anim.SetBool ("IsGround",IsGround);
+        }
+    }
+    private void OnTriggerExit2D (Collider2D col)
+    {
+       
+        
+
+        if (col.CompareTag ("Ground"))
+        {
+            IsGround = false;
+            anim.SetBool ("IsGround",IsGround);
+        }
+    }
+    #endregion
 
     #region 启用和禁用方法调用
     private void OnEnable ()
