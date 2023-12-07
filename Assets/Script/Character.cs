5edef549625a404635bf8678920771bd8254f076
QiuLaiHuo <yangbin_630@outlook.com>
2023-12-07 17:04:33 +0800

    2

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("»ù´¡ÊôĞÔ")]
+    public int Health;
+    public int CurrentHealth;
+    public int Shield;
+    public int CurrentShield;
+
+}
---
+++ Assets/Script/Player/AttackControll.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackControll : MonoBehaviour
+{
+    [Header ("¹¥»÷ÊôÐÔ")]
+    public int Damage;
+
+   
+
+   
+}
